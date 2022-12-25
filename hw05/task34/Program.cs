1e78a0862fd317eb21855355be28f5ccbb5cdefb
Kudrat <kudrat1101@mail.ru>
2022-12-26 00:32:20 +0300

    create hw5 with method

---
+++ hw05/task34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] arr = new int[5];
+
+int[] array(int[] qwery)
+{
+    Random rand = new Random();
+    int count = 0;
+
+    for (int i = 0; i < qwery.Length; i++)
+    {
+        qwery[i] = rand.Next(100, 1000);
+        if (qwery[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.Write(string.Join(", ", qwery));
+    System.Console.Write(" -> ");
+    Console.WriteLine(count);
+    System.Console.WriteLine();
+    return qwery;
+}
+System.Console.WriteLine(array(arr));
---
+++ hw05/task36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] arr = new int[6];
+
+int[] array(int[] qwery)
+{
+    Random rand = new Random();
+    int res = 0;
+
+    for (int i = 0; i < qwery.Length; i++)
+    {
+        qwery[i] = rand.Next(-9, 10);
+    }
+    for (int i = 1; i < qwery.Length; i += 2)
+    {
+        res = qwery[i] + res;
+    }
+    Console.WriteLine(string.Join(", ", qwery));
+    Console.WriteLine($"Сумма элементов array[1], [3], [5] = {res}");
+    return qwery;
+}
+System.Console.WriteLine(array(arr));
